95626e6953ff753720e8a0f7bb9fe776fa74668a
刘凌晨 <liulingchen1109@163.com>
2023-05-10 09:18:02 +0800

    Autofac and Filter

--- AutofacDemo-Api/Services/IService/IIocService.cs
+++
@@ -1,7 +0,0 @@
-namespace AutofacDemo_Api
-{
-    public interface IIocService
-    {
-        public IUserService UserService {get;set;}
-    }
-}
--- AutofacDemo-Api/Services/IService/IUserService.cs
+++
@@ -1,9 +0,0 @@
-namespace AutofacDemo_Api
-{
-    public interface IUserService
-    {
-        string GetUserName();
-
-        string GetUserAge();
-    }
-}
